f7db0c28678e5d069c5b299acaf06d00b2b365a8
temtechie <templendukwu144@gmail.com>
2023-03-21 13:15:43 +0100

    updated gitignore file

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express")
+const path = require("path")
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000
 
 const app = express();
 
 //Enable body parser
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use("/openai", require("./routes/openaiRoutes"))
 
